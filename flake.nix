0ca7dd19787ae123a5363c72c98c6b0d3897155b
Hossein Naderi <hossein-naderi@hotmail.com>
2023-01-20 11:51:32 +0330

    Enabled websockets in curl build

--- flake.nix
+++ flake.nix
@@ -12,6 +12,13 @@
           inherit system;
           overlays = [ typelevel-nix.overlay ];
         };
+        curl = pkgs.curl.overrideAttrs (old: {
+          # Websocket support in curl is currently in experimental mode
+          # which needs to be enabled explicitly in build time in order to be available
+          #
+          #https://github.com/curl/curl/blob/f8da4f2f2d0451dc0a126ae3e5077b4527ccdc86/configure.ac#L174
+          configureFlags = old.configureFlags ++ [ "--enable-websockets" ];
+        });
       in {
         devShell = pkgs.devshell.mkShell {
           imports = [ typelevel-nix.typelevelShell ];
@@ -20,7 +27,7 @@
             jdk.package = pkgs.jdk8;
             native = {
               enable = true;
-              libraries = [ pkgs.curl ];
+              libraries = [ curl ];
             };
           };
         };
