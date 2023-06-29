235d96963d18080eed14237a198bcf6186616fdd
Hossein Naderi <hossein-naderi@hotmail.com>
2023-06-29 14:53:29 +0330

    libcurl multi-socket method bindings

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val scala3 = "3.3.0"
   val scala213 = "2.13.11"
-  val catsEffectVersion = "3.5.1"
+  val catsEffectVersion = "3.6-e9aeb8c"
   val http4sVersion = "0.23.22"
   val munitCEVersion = "2.0.0-M3"
 }
