349f22f3b4a9ef108f6daa02f6ceba6a57978083
Hossein Naderi <hossein-naderi@hotmail.com>
2023-07-04 21:24:21 +0330

    Proper clean up for CurlMultiSocket

--- curl/src/main/scala/org/http4s/curl/unsafe/CurlMultiSocket.scala
+++ curl/src/main/scala/org/http4s/curl/unsafe/CurlMultiSocket.scala
@@ -31,14 +31,6 @@ import org.http4s.curl.internal._
 import scala.concurrent.duration._
 import scala.scalanative.unsafe._
 
-private[curl] trait CurlMultiSocket {
-  def addHandlerTerminating(easy: CurlEasy, cb: Either[Throwable, Unit] => Unit): IO[Unit]
-  def addHandlerNonTerminating(
-      easy: CurlEasy,
-      cb: Either[Throwable, Unit] => Unit,
-  ): Resource[IO, Unit]
-}
-
 private[curl] object CurlMultiSocket {
   implicit private class OptFibOps(private val f: Option[FiberIO[?]]) extends AnyVal {
     def cancel: IO[Unit] = f.fold(IO.unit)(_.cancel)
@@ -74,48 +66,11 @@ private[curl] object CurlMultiSocket {
     handle <- newCurlMutli
     fdPoller <- getFDPoller.toResource
     disp <- Dispatcher.sequential[IO]
-    mapping <- AtomicCell[IO].of(Map.empty[libcurl.curl_socket_t, Monitoring]).toResource
+    mapping <- AtomicCell[IO].of(State.empty).toResource
     timeout <- IO.ref[Option[FiberIO[Unit]]](None).toResource
     cms = new CurlMultiSocketImpl(handle, fdPoller, mapping, disp, timeout)
-    _ <- setup(cms, handle).toResource
+    _ <- cms.setup
   } yield cms
-
-  private def setup(cms: CurlMultiSocketImpl, handle: Ptr[libcurl.CURLM]) = IO {
-    val data = Utils.toPtr(cms)
-
-    libcurl
-      .curl_multi_setopt_timerdata(
-        handle,
-        libcurl_const.CURLMOPT_TIMERDATA,
-        data,
-      )
-      .throwOnError
-
-    libcurl
-      .curl_multi_setopt_socketdata(
-        handle,
-        libcurl_const.CURLMOPT_SOCKETDATA,
-        data,
-      )
-      .throwOnError
-
-    libcurl
-      .curl_multi_setopt_timerfunction(
-        handle,
-        libcurl_const.CURLMOPT_TIMERFUNCTION,
-        onTimeout(_, _, _),
-      )
-      .throwOnError
-
-    libcurl
-      .curl_multi_setopt_socketfunction(
-        handle,
-        libcurl_const.CURLMOPT_SOCKETFUNCTION,
-        onSocket(_, _, _, _, _),
-      )
-      .throwOnError
-
-  } *> cms.notifyTimeout
 
   final private case class Monitoring(
       read: Option[FiberIO[Nothing]],
@@ -124,6 +79,19 @@ private[curl] object CurlMultiSocket {
       unregister: IO[Unit],
   ) {
     def clean: IO[Unit] = IO.uncancelable(_ => read.cancel !> write.cancel !> unregister)
+  }
+
+  sealed private trait State
+  private object State {
+    val empty: State = Active()
+    final case class Active(monitors: Map[libcurl.curl_socket_t, Monitoring] = Map.empty)
+        extends State {
+      def add(fd: libcurl.curl_socket_t, monitor: Monitoring): Active = copy(
+        monitors.updated(fd, monitor)
+      )
+      def remove(fd: libcurl.curl_socket_t): Active = copy(monitors - fd)
+    }
+    case object Released extends State
   }
 
   private def onTimeout(
@@ -164,10 +132,72 @@ private[curl] object CurlMultiSocket {
   final private class CurlMultiSocketImpl(
       multiHandle: Ptr[libcurl.CURLM],
       fdpoller: FileDescriptorPoller,
-      mapping: AtomicCell[IO, Map[libcurl.curl_socket_t, Monitoring]],
+      mapping: AtomicCell[IO, State],
       disp: Dispatcher[IO],
       timeout: Ref[IO, Option[FiberIO[Unit]]],
   ) extends CurlMultiSocket {
+
+    private def init = IO {
+      val data = Utils.toPtr(this)
+
+      libcurl
+        .curl_multi_setopt_timerdata(
+          multiHandle,
+          libcurl_const.CURLMOPT_TIMERDATA,
+          data,
+        )
+        .throwOnError
+
+      libcurl
+        .curl_multi_setopt_socketdata(
+          multiHandle,
+          libcurl_const.CURLMOPT_SOCKETDATA,
+          data,
+        )
+        .throwOnError
+
+      libcurl
+        .curl_multi_setopt_timerfunction(
+          multiHandle,
+          libcurl_const.CURLMOPT_TIMERFUNCTION,
+          onTimeout(_, _, _),
+        )
+        .throwOnError
+
+      libcurl
+        .curl_multi_setopt_socketfunction(
+          multiHandle,
+          libcurl_const.CURLMOPT_SOCKETFUNCTION,
+          onSocket(_, _, _, _, _),
+        )
+        .throwOnError
+
+    } *> notifyTimeout
+
+    private def cleanup =
+      removeTimeoutIO !> mapping.evalUpdate {
+        case State.Active(monitors) =>
+          // First clean all monitors, this ensures that we don't call any
+          // curl callbacks afterwards, and callback cleaning and notifications
+          // is deterministic.
+          monitors.values.toList.traverse(_.clean) !> IO {
+            val error = new InterruptedException("Runtime shutdown!")
+
+            // Remove and notify all easy handles
+            // Note that we do this in mapping.evalUpdate in order to block
+            // other new usages while cleaning up
+            callbacks.foreach { case (easy, cb) =>
+              libcurl.curl_multi_remove_handle(multiHandle, easy).throwOnError
+              cb(Left(error))
+            }
+            callbacks.clear()
+          }.as(State.Released)
+        case State.Released =>
+          // It must not happen, but we leave a clue here if it happened!
+          IO.raiseError(new IllegalStateException("Cannot clean a released resource!"))
+      }
+
+    def setup: Resource[IO, Unit] = Resource.make(init)(_ => cleanup)
 
     private val callbacks =
       scala.collection.mutable.Map[Ptr[libcurl.CURL], Either[Throwable, Unit] => Unit]()
@@ -204,13 +234,16 @@ private[curl] object CurlMultiSocket {
         }
 
         IO.uncancelable(_ =>
-          mapping.evalUpdate { m =>
-            m.get(fd) match {
-              case None =>
-                newMonitor.map(m.updated(fd, _))
-              case Some(s: Monitoring) =>
-                s.clean *> newMonitor.map(m.updated(fd, _))
-            }
+          mapping.evalUpdate {
+            case state @ State.Active(monitors) =>
+              monitors.get(fd) match {
+                case None =>
+                  newMonitor.map(state.add(fd, _))
+                case Some(s: Monitoring) =>
+                  s.clean *> newMonitor.map(state.add(fd, _))
+              }
+            case State.Released =>
+              IO.raiseError(new IllegalStateException("Runtime is already closed!"))
           }
         )
       }
@@ -218,11 +251,14 @@ private[curl] object CurlMultiSocket {
     def remove(fd: libcurl.curl_socket_t): Unit =
       disp.unsafeRunAndForget(
         IO.uncancelable(_ =>
-          mapping.evalUpdate { m =>
-            m.get(fd) match {
-              case None => IO(m)
-              case Some(s) => s.clean.as(m - fd)
-            }
+          mapping.evalUpdate {
+            case state @ State.Active(monitors) =>
+              monitors.get(fd) match {
+                case None => IO(state)
+                case Some(s) => s.clean.as(state.remove(fd))
+              }
+            case State.Released =>
+              IO.raiseError(new IllegalStateException("Runtime is already closed!"))
           }
         )
       )
@@ -233,9 +269,8 @@ private[curl] object CurlMultiSocket {
       )
     )
 
-    def removeTimeout: Unit = disp.unsafeRunAndForget(
-      timeout.getAndSet(None).flatMap(_.cancel)
-    )
+    private def removeTimeoutIO = timeout.getAndSet(None).flatMap(_.cancel)
+    def removeTimeout: Unit = disp.unsafeRunAndForget(removeTimeoutIO)
 
     def notifyTimeout: IO[Unit] = IO {
       val running = stackalloc[Int]()
@@ -283,4 +318,12 @@ private[curl] object CurlMultiSocket {
     private def writeLoop(fd: libcurl.curl_socket_t, p: FileDescriptorPollHandle) =
       p.pollWriteRec(())(_ => action(fd, libcurl_const.CURL_CSELECT_OUT)).start
   }
+}
+
+private[curl] trait CurlMultiSocket {
+  def addHandlerTerminating(easy: CurlEasy, cb: Either[Throwable, Unit] => Unit): IO[Unit]
+  def addHandlerNonTerminating(
+      easy: CurlEasy,
+      cb: Either[Throwable, Unit] => Unit,
+  ): Resource[IO, Unit]
 }
